593acc78761eaa41bcebde083dc5c2db3cfcd830
naveenpahuja94 <naveenpahuja94@gmail.com>
2020-06-14 20:37:24 +0530

    matrix addition and subtraction using numpy package

---
+++ matrixOperationsPartOne.py
@@ -0,0 +1,15 @@
+# Importing the required package(s)
+import numpy as np
+
+# Initialize two matrices
+m1 = np.array([[1,2],[4,5]])
+m2 = np.array([[7,8],[9,10]])
+
+# Adding two matrices
+print("Addition of two matrices: ")
+print(np.add(m1,m2))
+
+# Subtracting two matrices
+print("Subrtacting two matrices: ")
+print(np.subtract(m1,m2))
+
